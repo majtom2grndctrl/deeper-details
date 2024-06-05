43a8a678787d4d335e82e34789d3e5f08ca755e5
Dan Hiester <dan.hiester@gmail.com>
2024-06-05 16:54:41 -0700

    Make animation work without adding scrollbar to content wrapper

--- src/deeper-details.ts
+++ src/deeper-details.ts
@@ -10,6 +10,9 @@ export class DeeperDetails extends LitElement {
   @property({ type: String })
   expandButtonLabel? = 'Show more'
 
+  @query('.content-wrapper')
+  contentWrapper: HTMLDivElement | undefined
+
   @query('slot[name=toggler-button]')
   togglerButtonSlot: HTMLSlotElement | undefined
 
@@ -19,22 +22,58 @@ export class DeeperDetails extends LitElement {
   @state()
   _showContent = false
 
+  private _handleToggleClick() {
+    const { contentWrapper } = this
+    const nextShowContentState = !this._showContent
+    if (!!nextShowContentState) {
+      this._showContent = nextShowContentState
+      contentWrapper?.classList.add('open-animation')
+      window.setTimeout(() => {
+        contentWrapper?.classList.remove('open-animation')
+      }, 450)
+    } else {
+      contentWrapper?.classList.add('init-close-animation')
+      window.setTimeout(() => {
+        contentWrapper?.classList.remove('init-close-animation')
+        contentWrapper?.classList.add('close-animation')
+      }, 1)
+      window.setTimeout(() => {
+        contentWrapper?.classList.remove('close-animation')
+        this._showContent = nextShowContentState
+      }, 450)
+    }
+  }
+
   static get styles() {
     return css`
+      .deeper-details-root {
+        border-top: var(--deeperDetails-borderStyle, 1px solid rgba(128,128,128,0.5));
+        border-bottom: var(--deeperDetails-borderStyle, 1px solid rgba(128,128,128,0.5));
+        padding: var(--deeperDetails-verticalPadding, 0.5em) var(--deeperDetails-horizontalPadding, 0);
+      }
       .button {
-        background-color: var(--button-bgColor, none);
-        border: var(--button-borderStyle, none);
-        color: var(--button-textColor, inherit);
+        background-color: var(--deeperDetails-button-bgColor, none);
+        border: var(--deeperDetails-button-borderStyle, none);
+        color: var(--deeperDetails-button-textColor, inherit);
         cursor: pointer;
-        font-size: var(--button-fontSize, 1em);
-        padding: var(--button-padding, 0);
+        font-size: var(--deeperDetails-button-fontSize, 1em);
+        padding: var(--deeperDetails-button-padding, 0.5em 0);
       }
       .content-wrapper {
-        max-height: 100vh;
-        overflow-y: auto;
-        transition: max-height 0.4s ease-in-out;
+        max-height: none;
+        overflow: hidden;
+        transition: var(--deeperDetails-content-transition, max-height 400ms ease-in-out);
+      }
+      .content-wrapper.open-animation {
+        max-height: 100lvh;
       }
       .content-wrapper[aria-hidden=true] {
+        max-height: 0;
+      }
+      .content-wrapper.init-close-animation {
+        max-height: 400lvh;
+      }
+      .content-wrapper.close-animation {
         max-height: 0;
       }
       .content-wrapper[aria-hidden=true] > .animation-wrapper {
@@ -45,16 +84,10 @@ export class DeeperDetails extends LitElement {
 
   firstUpdated() {
     const togglerButtonOverride = this.togglerButtonSlot?.assignedNodes()[0]
-    console.log(togglerButtonOverride);
     togglerButtonOverride?.addEventListener('click', () => this._handleToggleClick())
 
     const closeButtonOverride = this.closeButtonSlot?.assignedNodes()[0]
-    console.log(closeButtonOverride);
     closeButtonOverride?.addEventListener('click', () => this._handleToggleClick())
-  }
-
-  private _handleToggleClick() {
-    this._showContent = !this._showContent
   }
 
   render() {
@@ -70,7 +103,7 @@ export class DeeperDetails extends LitElement {
             <slot></slot>
           </div>
           <slot name="close-button">
-            <button @click=${this._handleToggleClick} class="button ">
+            <button @click=${this._handleToggleClick} class="button">
               ${this.closeButtonLabel}
             </button>
           </slot>
