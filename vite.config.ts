d088996b1f5a35cb10f4d263443814a3eb62cecb
Dan Hiester <dan.hiester@gmail.com>
2024-07-08 16:00:33 -0700

    Update build to include type definitions in dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/deeper-details.ts'),
       name: 'DeeperDetails',
-      fileName: 'deeper-details'
+      fileName: (format) => `deeper-details.${format}.js`
     },
-  }
+    outDir: 'dist',
+  },
+  plugins: [dts()],
 });
